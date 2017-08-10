e71207da289d5ea163d95ec881ab2d18f66e62a2
Felipe Samuel <samukajobs@gmail.com>
2017-08-10 15:21:06 -0300

    Update Upload.py

--- Upload.py
+++ Upload.py
@@ -1,15 +1,15 @@
-from CRM import app
-from CRM import log
+from seumodulo import app
+from seumodulo import log
 from flask import request
-import os
-import uuid
 from hurry.filesize import size
 from hurry.filesize import alternative
+import os
+import uuid
 
 class Upload(object):
 
     __imagens = ['.png','.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.bpg', '.ico', '.img', '.jps']
-    __documentos = ['.doc','.docx', '.pdf', '.txt', '.xls', '.ppt', '.odp', '.pot', '.pps', '.csv']
+    __documentos = ['.doc','.docx', '.pdf', '.txt', '.xls', '.ppt', '.odp', '.pot', '.pps', '.csv', '.rtf']
     __audios = ['.mp3', '.ogg', '.wma', '.aac','.ac3', '.wav', '.aa', '.aac', '.aiff']
     __videos = ['.mp4', '.avi', '.mpeg', '.mov', '.rmvb', '.mkv', '.wmv', '.webm', '.flv', '.vob', '.mpg', '.m4v', '.3gp']
     __comprimidos = ['.zip', '.rar', '.7z', '.iso', '.tar', '.bz2', '.gz', '.dmg', '.tar.gz', '.tgz']
@@ -112,68 +112,196 @@ class Upload(object):
 # Decorador
 def file_upload(func):
         def uploading(*args, **kwargs):
+            retorno = None
             try:
                 if request.method == 'POST':
-                    retorno = None
                     caminhos = []
-                    falhas = 0
-                    sucesso = 0
+                    quantidade_falhas = 0
+                    quantidade_sucesso = 0
                     total = 0
                     bytes_enviados = 0
+                    bytes_nao_enviados = 0
                     tamanho = 0
-                    for arquivo in request.files:
-                        if request.files[arquivo]:
-                            total += 1
-                            if tamanho <= app.config['MAX_SIZE_UPLOAD']:
-                                u = Upload(request.files[arquivo])
-                                if u.salvar():
-                                    tamanho = request.files[arquivo].seek(0,2)
-                                    caminhos.append(
-                                        {
-                                            "erro": False, 
-                                            "campo": arquivo, 
-                                            "caminho_completo": u.path,
-                                            "extensao": u.extensao,
-                                            "tamanho": size(tamanho, system=alternative),
-                                            "nome_arquivo": u.nome,
-                                            "msg": "Arquivo enviado com sucesso."
-                                        })
-                                    sucesso += 1
-                                    bytes_enviados += tamanho
-                                else:
-                                    caminhos.append(
-                                        {
-                                            "erro": True, 
-                                            "campo": arquivo, 
-                                            "msg": u.erro
-                                        })
-                                    falhas +=1 
-                            else:
-                                caminhos.append(
+                    qtd = len(request.files)
+                    if qtd > app.config['MAX_FILE_UPLOAD']:
+                        retorno = {
+                                "erro": True,
+                                 "msg": "Quantidade máxima de arquivos excedida."
+                                }
+                    else:
+                        for arquivo in request.files:
+                            if request.files[arquivo]:
+                                total += 1
+                                tamanho = tamanho_arquivo(request.files[arquivo])
+                                if tamanho <= app.config['MAX_SIZE_UPLOAD']:
+                                    u = Upload(request.files[arquivo])
+                                    if u.salvar():
+                                        caminhos.append(
+                                            {
+                                                "erro": False, 
+                                                "campo": arquivo, 
+                                                "caminho_completo": u.path,
+                                                "extensao": u.extensao,
+                                                "tamanho": size(tamanho, system=alternative),
+                                                "nome_arquivo": u.nome,
+                                                "msg": "Arquivo enviado com sucesso."
+                                            })
+                                        quantidade_sucesso += 1
+                                        bytes_enviados += tamanho
+                                    else:
+                                        caminhos.append(
                                             {
                                                 "erro": True, 
+                                                "tamanho": size(tamanho, system=alternative),
+                                                "nome_arquivo": request.files[arquivo].filename,
                                                 "campo": arquivo, 
-                                                "msg": "O arquivo excede o tamanho máximo permitido."
+                                                "msg": u.erro
                                             })
-                                falhas +=1 
+                                        bytes_nao_enviados = tamanho
+                                        quantidade_falhas +=1 
+                                else:
+                                    caminhos.append(
+                                                {
+                                                    "erro": True, 
+                                                    "campo": arquivo, 
+                                                    "tamanho": size(tamanho, system=alternative),
+                                                    "nome_arquivo": request.files[arquivo].filename,
+                                                    "msg": "O arquivo excede o tamanho máximo permitido."
+                                                })
+                                    bytes_nao_enviados = tamanho
+                                    quantidade_falhas +=1 
 
-                    porcentagem_falhas = 0
-                    porcentagem_sucessos = 0
-                    if total != 0:
-                        porcentagem_falhas =  round((100 * float(falhas) / float (total)), 2)
-                        porcentagem_sucessos = round((100 * float(sucesso) / float (total)), 2)
-                    retorno = {
-                        "quantidade_falhas": falhas, 
-                        "quantidade_sucessos": sucesso, 
-                        "arquivos": caminhos,
-                        "arquivos_enviados": total,
-                        "bytes_enviados": size(bytes_enviados, system=alternative),
-                        "porcentagem_de_falha": porcentagem_falhas,
-                        "porcentagem_de_sucesso": porcentagem_sucessos,
-                        "msg": str(porcentagem_sucessos)+'% dos arquivos foram enviados com sucesso.' if not caminhos == [] else "Os campos dos arquivos estão todos vazios."
-                        }
+                        porcentagem_falhas = 0
+                        porcentagem_sucessos = 0
+                        if total != 0:
+                            porcentagem_falhas =  round((100 * float(quantidade_falhas) / float (total)), 2)
+                            porcentagem_sucessos = round((100 * float(quantidade_sucesso) / float (total)), 2)
+                        retorno = {
+                            "quantidade_falhas": quantidade_falhas, 
+                            "quantidade_sucessos": quantidade_sucesso, 
+                            "arquivos": caminhos,
+                            "arquivos_enviados": total,
+                            "bytes_enviados": size(bytes_enviados, system=alternative),
+                            "bytes_nao_enviados": size(bytes_nao_enviados, system = alternative),
+                            "porcentagem_de_falha": porcentagem_falhas,
+                            "porcentagem_de_sucesso": porcentagem_sucessos,
+                            "tamanho_maximo_permitido": size(app.config['MAX_SIZE_UPLOAD'], system = alternative),
+                            "quantidade_maxima_permitida": app.config['MAX_FILE_UPLOAD'],
+                            "msg": str(porcentagem_sucessos)+'% dos arquivos foram enviados com sucesso.' if not caminhos == [] else "Os campos dos arquivos estão todos vazios."
+                            }
                     return func(retorno, *args, **kwargs)
             except:
                 log.logging()
-            return func(*args, **kwargs)
+                retorno = {
+                    "erro": True, 
+                    "msg": "Falha ao realizar o upload dos arquivos."
+                    }
+            return func(retorno, *args, **kwargs)
         return uploading
+
+# Decorador
+def multi_file_upload(func):
+        def uploading(*args, **kwargs):
+            retorno = None
+            try:
+                if request.method == 'POST':
+                    caminhos = []
+                    quantidade_falhas = 0
+                    quantidade_sucesso = 0
+                    total = 0
+                    bytes_enviados = 0
+                    bytes_nao_enviados = 0
+                    tamanho = 0
+                    for up in request.files:
+                        uploaded_files = request.files.getlist(up)
+                        qtd = len(uploaded_files)
+                        if qtd > app.config['MAX_FILE_UPLOAD']:
+                            retorno = {
+                                "erro": True,
+                                "msg": "Quantidade máxima de arquivos excedida."
+                                }
+                        else:
+                            for file in uploaded_files:
+                                if file:
+                                    total += 1
+                                    tamanho = tamanho_arquivo(file)
+                                    if tamanho <= app.config['MAX_SIZE_UPLOAD']:
+                                        u = Upload(file)
+                                        if u.salvar():
+                                            caminhos.append(
+                                                {
+                                                    "erro": False, 
+                                                    "campo": up, 
+                                                    "caminho_completo": u.path,
+                                                    "extensao": u.extensao,
+                                                    "tamanho": size(tamanho, system=alternative),
+                                                    "nome_arquivo": u.nome,
+                                                    "msg": "Arquivo enviado com sucesso."
+                                                })
+                                            quantidade_sucesso += 1
+                                            bytes_enviados += tamanho
+                                        else:
+                                            caminhos.append(
+                                                {
+                                                    "erro": True, 
+                                                    "campo": up, 
+                                                    "msg": u.erro,
+                                                    "tamanho": size(tamanho, system=alternative),
+                                                    "nome_arquivo": file.filename
+                                                })
+                                            bytes_nao_enviados = tamanho
+                                            quantidade_falhas +=1 
+                                    else:
+                                        caminhos.append(
+                                                    {
+                                                        "erro": True, 
+                                                        "campo": up, 
+                                                        "tamanho": size(tamanho, system=alternative),
+                                                        "nome_arquivo": file.filename,
+                                                        "msg": "O arquivo excede o tamanho máximo permitido."
+                                                    })
+                                        bytes_nao_enviados = tamanho
+                                        quantidade_falhas +=1 
+
+                            porcentagem_falhas = 0
+                            porcentagem_sucessos = 0
+                            if total != 0:
+                                porcentagem_falhas =  round((100 * float(quantidade_falhas) / float (total)), 2)
+                                porcentagem_sucessos = round((100 * float(quantidade_sucesso) / float (total)), 2)
+                            retorno = {
+                                "quantidade_falhas": quantidade_falhas, 
+                                "quantidade_sucessos": quantidade_sucesso, 
+                                "arquivos": caminhos,
+                                "arquivos_enviados": total,
+                                "bytes_enviados": size(bytes_enviados, system=alternative),
+                                "bytes_nao_enviados": size(bytes_nao_enviados, system=alternative),
+                                "porcentagem_de_falha": porcentagem_falhas,
+                                "porcentagem_de_sucesso": porcentagem_sucessos,
+                                "tamanho_maximo_permitido": size(app.config['MAX_SIZE_UPLOAD'], system = alternative),
+                                "quantidade_maxima_permitida": app.config['MAX_FILE_UPLOAD'],
+                                "msg": str(porcentagem_sucessos)+'% dos arquivos foram enviados com sucesso.' if not caminhos == [] else "Os campos dos arquivos estão todos vazios."
+                                }
+                    return func(retorno, *args, **kwargs)
+            except:
+                log.logging()
+                retorno = {
+                    "erro": True, 
+                    "msg": "Falha ao realizar o upload dos arquivos."
+                    }
+            return func(retorno, *args, **kwargs)
+        return uploading
+
+# Retorna o tamnanho de um arquivo 
+def tamanho_arquivo(fobj):
+    if fobj.content_length:
+        return fobj.content_length
+    try:
+        pos = fobj.tell()
+        fobj.seek(0, 2)
+        size = fobj.tell()
+        fobj.seek(pos)
+        return size
+    except (AttributeError, IOError):
+        log.logging()
+        pass
+    return 0
